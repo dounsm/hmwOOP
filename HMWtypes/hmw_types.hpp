e45b8715d0916228aeec50a3039b8377bbdd6010
LapTop - HONOR <doudounsmiao2008@dingtalk.com>
2025-07-16 22:09:28 +0800

    add files

---
+++ HMWtypes/hmw_types.hpp
@@ -0,0 +1,24 @@
+#ifndef __HMW_TYPES____
+#define __HMW_TYPES____
+
+#include <vector>
+
+namespace NN{
+    class Neuron{
+
+    };
+
+    class Layer{
+
+    };
+
+    class Network{
+
+    };
+
+    class ActivationFunction{
+        
+    };
+};
+
+#endif
